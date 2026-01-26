7cb4316b3d7cc20fc3894359164db663134e79ef
sluka <sluka@foundation.sk>
2026-01-26 20:09:30 +0100

    another fix

--- i18n.ts
+++ i18n.ts
@@ -5,10 +5,13 @@ export const locales = ['en', 'sk', 'de'] as const;
 export const defaultLocale = 'en' as const;
 export type Locale = (typeof locales)[number];
 
-export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale as Locale)) notFound();
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale;
+  
+  if (!locale || !locales.includes(locale as Locale)) notFound();
 
   return {
+    locale,
     messages: (await import(`./messages/${locale}.json`)).default
   };
 });
